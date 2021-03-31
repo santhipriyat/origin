efdb7e6f1ca26707091ca7981a469709ce5cd24a
santhipriyat <70535452+santhipriyat@users.noreply.github.com>
2021-03-31 15:39:55 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,30 @@
+resource "azurerm_resource_group" "slotDemo" {
+  name     = "slotDemo1"
+  location = "westus2"
+}
+
+resource "azurerm_app_service_plan" "slotDemo" {
+  name                = "slotAppServicePlan"
+  location            = azurerm_resource_group.slotDemo.location
+  resource_group_name = azurerm_resource_group.slotDemo.name
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "random_id" "randomId" {
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = azurerm_resource_group.slotDemo.name
+  }
+
+  byte_length = 9
+}
+resource "azurerm_app_service" "slotDemo" {
+  name                = "slotAppService${random_id.randomId.hex}"
+  location            = azurerm_resource_group.slotDemo.location
+  resource_group_name = azurerm_resource_group.slotDemo.name
+  app_service_plan_id = azurerm_app_service_plan.slotDemo.id
+}
+
---
+++ provider.tf
@@ -0,0 +1,10 @@
+provider "azurerm" {
+  version         = "=2.4.0"
+  subscription_id = "498d82a4-7592-4530-b3bd-ed8b27c97bb0"
+  client_id       = "c0c59dd6-d795-408a-8a1a-6e082306d1b2"
+  client_secret   = "uV5J6x-oV_2~daMC1pMaoM1-a-3y8rnJm6"
+  tenant_id       = "b6cd224b-dc37-4293-a8c8-52c0f2944281"
+  features {}
+}
+
+  
